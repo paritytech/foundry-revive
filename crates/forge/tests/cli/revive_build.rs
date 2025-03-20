950dcfc1b6d3f477d35c9904e1d00769c40747fd
Sebastian Miasojed <sebastian.miasojed@parity.io>
2025-03-20 12:02:44 +0100

    Add test for contract sizes

--- crates/forge/tests/cli/revive_build.rs
+++ crates/forge/tests/cli/revive_build.rs
@@ -1,4 +1,5 @@
 use crate::utils::generate_large_init_contract;
+use foundry_test_utils::{forgetest, snapbox::IntoData, str};
 use foundry_compilers::artifacts::BytecodeHash;
 use foundry_config::Config;
 
@@ -14,10 +15,34 @@ forgetest_init!(force_buid_with_revive, |prj, cmd| {
     cmd.args(["build", "--revive-compile", "--force"]).assert_success();
 });
 
-forgetest!(initcode_size_limit_can_be_ignored_revive, |prj, cmd| {
-    // BytecodeHash issue workaround https://github.com/paritytech/revive/issues/219
+forgetest!(code_size_exceeds_limit_with_revive, |prj, cmd| {
     prj.write_config(Config { bytecode_hash: BytecodeHash::None, ..Default::default() });
-    // This test won't work until this issue is fixed:https://github.com/paritytech/revive/issues/172
-    prj.add_source("LargeContract", generate_large_init_contract(249_000).as_str()).unwrap();
-    cmd.args(["build", "--revive-compile", "--sizes", "--ignore-eip-3860"]).assert_success();
+    prj.add_source("LargeContract.sol", generate_large_init_contract(25_000).as_str()).unwrap();
+    cmd.args(["build", "--revive-compile", "--sizes"]).assert_failure().stdout_eq(str![[r#"
+[COMPILING_FILES] with resolc and [SOLC_VERSION]
+resolc and [SOLC_VERSION] [ELAPSED]
+Compiler run successful!
+
+╭---------------+------------------+-------------------+--------------------+---------------------╮
+| Contract      | Runtime Size (B) | Initcode Size (B) | Runtime Margin (B) | Initcode Margin (B) |
++=================================================================================================+
+| LargeContract | 268,040          | 268,040           | -18,040            | -18,040             |
+╰---------------+------------------+-------------------+--------------------+---------------------╯
+
+
+"#]]);
+
+    cmd.forge_fuse().args(["build", "--revive-compile", "--sizes", "--json"]).assert_failure().stdout_eq(
+        str![[r#"
+{
+  "LargeContract": {
+    "runtime_size": 268040,
+    "init_size": 268040,
+    "runtime_margin": -18040,
+    "init_margin": -18040
+  }
+}
+"#]]
+        .is_json(),
+    );
 });
