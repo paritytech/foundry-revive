d9245c40eb6910ed8cef48fda93ab68968ccc0cb
Sebastian Miasojed <sebastian.miasojed@parity.io>
2025-03-20 14:38:22 +0100

    Clippy

--- crates/forge/tests/cli/revive_build.rs
+++ crates/forge/tests/cli/revive_build.rs
@@ -1,7 +1,5 @@
-use crate::utils::generate_large_init_contract;
 use foundry_compilers::artifacts::BytecodeHash;
 use foundry_config::Config;
-use foundry_test_utils::{forgetest, snapbox::IntoData, str};
 
 forgetest_init!(can_build_with_revive, |prj, cmd| {
     // BytecodeHash issue workaround https://github.com/paritytech/revive/issues/219
