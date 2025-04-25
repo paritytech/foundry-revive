25a45bc4c7fc1f976ac06d03688a13ad3f9a2cfb
Sebastian Miasojed <sebastian.miasojed@parity.io>
2025-04-25 09:24:36 +0200

    Rename revive to resolc

--- crates/forge/tests/cli/revive_build.rs
+++ crates/forge/tests/cli/revive_build.rs
@@ -2,19 +2,19 @@ use foundry_test_utils::snapbox::IntoData;
 
 use crate::utils::generate_large_init_contract;
 
-forgetest_init!(can_build_with_revive, |prj, cmd| {
-    cmd.args(["build", "--revive-compile"]).assert_success();
+forgetest_init!(can_build_with_resolc, |prj, cmd| {
+    cmd.args(["build", "--resolc-compile"]).assert_success();
 });
 
-forgetest_init!(force_buid_with_revive, |prj, cmd| {
-    cmd.args(["build", "--revive-compile", "--force"]).assert_success();
+forgetest_init!(force_buid_with_resolc, |prj, cmd| {
+    cmd.args(["build", "--resolc-compile", "--force"]).assert_success();
 });
 
-forgetest!(code_size_exceeds_limit_with_revive, |prj, cmd| {
+forgetest!(code_size_exceeds_limit_with_resolc, |prj, cmd| {
     prj.add_source("LargeContract.sol", generate_large_init_contract(50_000).as_str()).unwrap();
-    cmd.args(["build", "--revive-compile", "--sizes"]).assert_failure().stdout_eq(str![[r#"
-[COMPILING_FILES] with [REVIVE_VERSION]
-[REVIVE_VERSION] [ELAPSED]
+    cmd.args(["build", "--resolc-compile", "--sizes"]).assert_failure().stdout_eq(str![[r#"
+[COMPILING_FILES] with [RESOLC_VERSION]
+[RESOLC_VERSION] [ELAPSED]
 Compiler run successful!
 
 ╭---------------+------------------+-------------------+--------------------+---------------------╮
@@ -27,7 +27,7 @@ Compiler run successful!
 "#]]);
 
     cmd.forge_fuse()
-        .args(["build", "--revive-compile", "--sizes", "--json"])
+        .args(["build", "--resolc-compile", "--sizes", "--json"])
         .assert_failure()
         .stdout_eq(
             str![[r#"
