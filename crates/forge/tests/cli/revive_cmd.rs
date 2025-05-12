68ac72bfbf34f83c7d8a3d03fba14904782a8c6c
filip <filip.baciu@parity.io>
2025-05-12 16:55:31 +0300

    Merge branch 'master' into sm/path

--- crates/forge/tests/cli/revive_cmd.rs
+++ crates/forge/tests/cli/revive_cmd.rs
@@ -1,3 +1,7 @@
+use std::str::FromStr;
+
+use foundry_compilers::artifacts::Remapping;
+
 use crate::constants::*;
 // checks that `clean` works
 forgetest_init!(can_clean_config, |prj, cmd| {
@@ -47,5 +51,89 @@ Compiler run successful!
 [SOLC_VERSION] [ELAPSED]
 Compiler run successful!
 
+"#]]);
+});
+
+// Tests that direct import paths are handled correctly
+forgetest!(can_handle_direct_imports_into_src_for_resolc, |prj, cmd| {
+    prj.add_source(
+        "Foo",
+        r#"
+import {FooLib} from "src/FooLib.sol";
+struct Bar {
+    uint8 x;
+}
+contract Foo {
+    mapping(uint256 => Bar) bars;
+    function checker(uint256 id) external {
+        Bar memory b = bars[id];
+        FooLib.check(b);
+    }
+    function checker2() external {
+        FooLib.check2(this);
+    }
+}
+   "#,
+    )
+    .unwrap();
+
+    prj.add_source(
+        "FooLib",
+        r#"
+import {Foo, Bar} from "src/Foo.sol";
+library FooLib {
+    function check(Bar memory b) internal {}
+    function check2(Foo f) internal {}
+}
+   "#,
+    )
+    .unwrap();
+
+    cmd.args(["build", "--resolc"]).assert_success().stdout_eq(str![[r#"
+[COMPILING_FILES] with [RESOLC_VERSION]
+[RESOLC_VERSION] [ELAPSED]
+Compiler run successful!
+"#]]);
+});
+
+forgetest!(can_use_absolute_imports_for_resolc, |prj, cmd| {
+    prj.update_config(|config| {
+        let remapping = prj.paths().libraries[0].join("myDependency");
+        config.remappings =
+            vec![Remapping::from_str(&format!("myDependency/={}", remapping.display()))
+                .unwrap()
+                .into()];
+    });
+
+    prj.add_lib(
+        "myDependency/src/interfaces/IConfig.sol",
+        r"
+    interface IConfig {}
+   ",
+    )
+    .unwrap();
+
+    prj.add_lib(
+        "myDependency/src/Config.sol",
+        r#"
+        import "src/interfaces/IConfig.sol";
+    contract Config {}
+   "#,
+    )
+    .unwrap();
+
+    prj.add_source(
+        "Greeter",
+        r#"
+        import "myDependency/src/Config.sol";
+    contract Greeter {}
+   "#,
+    )
+    .unwrap();
+
+    cmd.args(["build", "--resolc"]).assert_success().stdout_eq(str![[r#"
+[COMPILING_FILES] with [RESOLC_VERSION]
+[RESOLC_VERSION] [ELAPSED]
+Compiler run successful!
 "#]]);
 });
